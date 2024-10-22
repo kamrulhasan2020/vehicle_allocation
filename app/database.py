12e92c484a0fb951d5a678ee2e1c63490f00adad
Kamrul <kkamrulhasan2020@gmail.com>
2024-10-22 22:48:50 +0600

    added docstrings for database code

--- app/database.py
+++ app/database.py
@@ -1,12 +1,19 @@
 import aioredis
 import motor.motor_asyncio
 
-MONGO_URI = "mongodb://mongo:27017"
+# MongoDB connection URL
+MONGO_URL = "mongodb://mongo:27017"
+# Redis connection URL
 REDIS_URL = "redis://redis:6379"
 
-client = motor.motor_asyncio.AsyncIOMotorClient(MONGO_URI)
+# Create an asynchronous MongoDB client
+client = motor.motor_asyncio.AsyncIOMotorClient(MONGO_URL)
 
+# Access the 'vehicle_allocation' database from MongoDB
 database = client.vehicle_allocation
 
+# Access the 'allocations' collection within the database
 allocations_collection = database.get_collection("allocations")
+
+# Create an asynchronous Redis client with UTF-8 encoding and response decoding
 redis = aioredis.from_url(REDIS_URL, encoding="utf-8", decode_responses=True)
